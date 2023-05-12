436283e492d62bded306ebcc6057a251bcf8a372
ol-kis <99480768+ol-kis@users.noreply.github.com>
2023-05-12 10:42:22 +0300

    Update 10.05.2023.py
    с возможностью изменения размера окна

--- example/10.05.2023.py
+++ example/10.05.2023.py
@@ -4,15 +4,16 @@ class SubWindow(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
         x = tk.Text(self)
-        x.pack()
+        x.pack(expand=1, fill='both')
+        
 
 class MainWindow(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self)
         self.win1 = SubWindow(self)
-        self.win1.pack(side="left")
+        self.win1.pack(side="left", expand=1, fill=tk.BOTH)
         self.win2 = SubWindow(self)
-        self.win2.pack(side="right")
+        self.win2.pack(side="right", expand=1, fill=tk.BOTH)
 
 if __name__ == "__main__":
     main = MainWindow()
