79dfb0c860a7c0b492678fdc433347e67641a3d9
ol-kis <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>
2023-05-14 18:47:10 +0300

    new version 14.05.2023

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-from PIL import ImageTk, Image
 import os
-import openpyxl
+# import openpyxl
 import datetime
 import time
-
+from itertools import cycle
 from tkinter import *
+from multiprocessing import Process
 
 
 # from tkinter import ttk
@@ -14,55 +14,80 @@ from tkinter import *
 class windows(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.wm_title("Test Application")
-        self.configure(background='yellow')
+        self.wm_title("Федерация функционального многоборья")
+        # self.configure(background='yellow')
 
 
-class atlets():
-    def __init__(self, window):
+class Atlets(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
         self.atlets_now = tk.Label(text=f'Hello', fg="black", bg="pink", width=50, height=10,
                                    font=("Times New Roman", 14), justify="left", )
         self.atlets_next = tk.Label(text=f'Hello', fg="black", bg="red", width=50, height=10,
                                     font=("Times New Roman", 14), justify="left", )
-        self.atlets_now.pack(padx=10, pady=10)
-        self.atlets_next.pack(padx=10, pady=10)
+        self.atlets_now.pack(side=RIGHT, padx=5, pady=5, anchor="n")
+        self.atlets_next.pack(side=RIGHT, padx=5, expand=True, anchor="s")
 
-    def update_atlets(self, window, sheet):
+    def update_atlets(self, window):
         for i in range(12, 50, 5):
-            self.atlets_now["text"] = f'\nЗаход:{sheet.cell(row=i, column=2).value} \
-                \nСейчас на поле следующие атлеты: \n\t{str(sheet.cell(row=i + 1, column=3).value)}\
-                                                   \n\t{str(sheet.cell(row=i + 2, column=3).value)}\
-                                                   \n\t{str(sheet.cell(row=i + 3, column=3).value)}\
-                                                   \n\t{str(sheet.cell(row=i + 4, column=3).value)}'
+            # self.atlets_now["text"] = f'\nЗаход:{sheet.cell(row=i, column=2).value} \
+            #     \nСейчас на поле следующие атлеты: \n\t{str(sheet.cell(row=i + 1, column=3).value)}\
+            #                                         \n\t{str(sheet.cell(row=i + 2, column=3).value)}\
+            #                                         \n\t{str(sheet.cell(row=i + 3, column=3).value)}\
+            #                                         \n\t{str(sheet.cell(row=i + 4, column=3).value)}'
+            self.atlets_now["text"] = f'New {i}'
             self.atlets_next["text"] = f'Old {i}'
-            time.sleep(5)
+            time.sleep(2)
             window.update()
         window.mainloop()
 
-    def get_list_of_athletes(self):
-        wb = openpyxl.load_workbook(filename='Тайминг 23 апреля.xlsx', data_only=True)
-        sheet = wb["Лист1"]
-        return sheet
+    # def get_list_of_athletes(self):
+    #     wb = openpyxl.load_workbook(filename='Тайминг 23 апреля.xlsx', data_only=True)
+    #     sheet = wb["Лист1"]
+    #     return sheet
 
 
-class Logo():
-    def __init__(self, window):
-        canvas = Canvas(window, width=500, height=500)
-        canvas.pack()
-        img = PhotoImage(file='attachment.png')
-        canvas.create_image(10, 10, anchor=NW, image=img)
-        window.mainloop()
+class App(tk.Frame):
+    def __init__(self, parent, controller, image_files, delay):
+        tk.Frame.__init__(self, parent)
+        self.delay = delay
+        self.pictures = cycle((tk.PhotoImage(file=image), image)
+                              for image in image_files)
+        self.picture_display = tk.Label(controller)
+        self.picture_display.pack()
 
+    def show_slides(self):
+        img_object, img_name = next(self.pictures)
+        self.picture_display.config(image=img_object)
+        self.after(self.delay, self.show_slides)
+
+    def run(self):
+        self.mainloop()
 
 
 if __name__ == "__main__":
     testObj = windows()
-    Logo = Logo(testObj)
-    l = atlets(testObj)
-    sheet = l.get_list_of_athletes()
-    l.update_atlets(testObj, sheet)
+    container = tk.Frame(testObj, height=600, width=600)
+    container.pack()
+    array = Atlets(container, testObj)
+    frame = array
 
+    image_files = [
+        'sticker_vk_bestsummerever_020.gif',
+        'sticker_vk_bestsummerever_021.gif',
+        'sticker_vk_bestsummerever_022.gif',
+        'sticker_vk_bestsummerever_023.gif',
+        'sticker_vk_bestsummerever_024.gif'
+    ]
+    delay = 3500
+    app = App(container, testObj, image_files, delay)
+    frame = app
+    frame.pack()
 
-    # sheet=get_workbook()
-    # x=12
-    # print(f'\nЗаход:{sheet.cell(row=x, column=2).value} \nСейчас на поле следующие атлеты: \n\t{str(sheet.cell(row=x, column=3).value)}')
+    p1 = Process(target=app.show_slides())
+    p1.start()
+    p2 = Process(target=array.update_atlets(testObj))
+    p2.start()
+    p1.join()
+    p2.join()
+
